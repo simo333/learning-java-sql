c56ef07101aca7d71ea93096b52e68208f27d547
Simo <szymon.333@wp.pl>
2022-04-27 18:51:36 +0200

    Update #5 Added task 4 in packages: * modification

--- src/main/java/pl/mysql/modification/Main04.java
+++ src/main/java/pl/mysql/modification/Main04.java
@@ -1,5 +1,10 @@
 package pl.mysql.modification;
 
 public class Main04 {
-
+    public static void main(String[] args) {
+        String queryChangeName = "UPDATE cinemas SET address = 'Stadion Narodowy' WHERE name LIKE '%z'";
+        String queryDeletePay = "DELETE FROM payments WHERE DATEDIFF(CURRENT_DATE, payment_date) > 4";
+        String queryChangeRating = "UPDATE movies SET rating = 5.4 WHERE LENGTH(description) > 40";
+        String queryLowerTicketPrice = "UPDATE tickets SET price = price * 0.5 WHERE quantity > 10";
+    }
 }

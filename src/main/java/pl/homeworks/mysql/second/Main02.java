fecb84281e0dd1555380829671b84e9a83b73994
Simo <szymon.333@wp.pl>
2022-04-28 22:05:12 +0200

    Update #6.2 Homework part II: Task 2

--- src/main/java/pl/homeworks/mysql/second/Main02.java
+++ src/main/java/pl/homeworks/mysql/second/Main02.java
@@ -1,8 +1,8 @@
 package pl.homeworks.mysql.second;
 
 public class Main02 {
-    public String queryRelationImages = "";
+    public String queryRelationImages = "ALTER TABLE images ADD FOREIGN KEY (offer_id) REFERENCES offers(id)";
 
-    public String queryRelationUsersCompanies = "";
+    public String queryRelationUsersCompanies = "ALTER TABLE users_companies ADD FOREIGN KEY (user_id) REFERENCES users(id)";
 
 }

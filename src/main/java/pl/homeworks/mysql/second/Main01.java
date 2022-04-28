0aaf3ba5ce480ffa1572b97ab362c09402809f7e
Simo <szymon.333@wp.pl>
2022-04-28 20:34:47 +0200

    Update #6.1 Homework part II: Task 1

--- src/main/java/pl/homeworks/mysql/second/Main01.java
+++ src/main/java/pl/homeworks/mysql/second/Main01.java
@@ -1,14 +1,14 @@
 package pl.homeworks.mysql.second;
 
 public class Main01 {
-       public String query1 = "";
+       public String query1 = "UPDATE offers SET price = price + 1000 WHERE id BETWEEN 20 AND 25";
 
-       public String query2 = "";
+       public String query2 = "DELETE FROM offers WHERE DATEDIFF(NOW(), expire) >= 3";
 
-       public String query3 = "";
+       public String query3 = "DELETE FROM offers WHERE phone LIKE '+48%' AND promoted = 1 AND DATEDIFF(expire, NOW()) > 0";
 
-       public String query4 = "";
+       public String query4 = "UPDATE offers SET promoted = 1, promoted_to = DATE_ADD(promoted_to, INTERVAL 23 DAY) WHERE SUBSTRING(phone FROM 3 FOR 2) = '48'";
 
-       public String query5 = "";
+       public String query5 = "UPDATE offers SET description = REPLACE(description, 'executed', 'found') WHERE phone LIKE '%(%)%' AND price > 400000";
 
 }

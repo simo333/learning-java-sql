3095d77725179aa601376e9be37285977d499992
Simo <szymon.333@wp.pl>
2022-04-29 18:19:39 +0200

    Update #6.3 Homework part II: Tasks 3,4

--- src/main/java/pl/homeworks/mysql/second/Main03.java
+++ src/main/java/pl/homeworks/mysql/second/Main03.java
@@ -1,5 +1,15 @@
 package pl.homeworks.mysql.second;
 
 public class Main03 {
+    public static void main(String[] args) {
+        String queryCreateTableScreenings = "CREATE TABLE screenings (\n" +
+                "    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,\n" +
+                "    cinema_id INT,\n" +
+                "    movie_id INT,\n" +
+                "    show_time DATETIME,\n" +
+                "    FOREIGN KEY (cinema_id) REFERENCES cinemas(id),\n" +
+                "    FOREIGN KEY (movie_id) REFERENCES movies(id)\n" +
+                ")";
+    }
 
 }
--- src/main/java/pl/homeworks/mysql/second/Main04.java
+++ src/main/java/pl/homeworks/mysql/second/Main04.java
@@ -1,5 +1,8 @@
 package pl.homeworks.mysql.second;
 
 public class Main04 {
+    public static void main(String[] args) {
+        String queryMoviesList = "SELECT * FROM movies LEFT JOIN cinemas_ex.screenings s on movies.id = s.movie_id";
+    }
 
 }

83ba4177159b09f1e215f5f874a44fc156c27999
Simo <szymon.333@wp.pl>
2022-05-01 20:35:27 +0200

    Update #6.4 Homework part II: Task 5

--- src/main/java/pl/homeworks/mysql/second/Main05.java
+++ src/main/java/pl/homeworks/mysql/second/Main05.java
@@ -1,5 +1,12 @@
 package pl.homeworks.mysql.second;
 
 public class Main05 {
+    public static void main(String[] args) {
+        String queryAllCinemas = "SELECT * FROM cinemas";
+        String queryAllMoviesByCinemaId =
+                "SELECT movies.* FROM movies LEFT JOIN cinemas_movies cm ON movies.id = cm.movie_id WHERE cinema_id = ?";
+        String queryAllScreenings =
+                "SELECT screenings.* FROM screenings LEFT JOIN cinemas c on screenings.cinema_id = c.id";
 
+    }
 }
